dd43f6dbf179c1e5925f6b0457f1bc4bd41ccb1c
HebaElgohary <hebasamir535@gmail.com>
2026-05-04 18:24:46 +0300

    chore: add .evn file

---
+++ src/config/env.ts
@@ -0,0 +1,10 @@
+export const ENV = {
+    DEV_MODE: 'development',
+    API_URL: 'http://localhost:3005',
+    API_TIMEOUT: 5000,
+    APP_NAME: 'SurveyLand',
+    APP_VERSION: '1.0.0',
+    ACESS_TOKEN_KEY: 'accessToken',
+    REFRESH_TOKEN_KEY: 'refreshToken',
+    DISABLE_DASHBOARD_PROTECTION: false,
+};
---
+++ src/services/apiClient.ts
@@ -0,0 +1,5 @@
+export const apiClient = {
+    get:async(url: string) => {
+        const response = await fetch(url);
+        return response.json();
+    }
